7f84cd17aaf7179733207578420ad2ca1f85e428
Jan Lolling <jan.lolling@gmail.com>
2016-12-19 22:21:06 +0100

    package name changed

--- src/de/cimt/talendcomp/db2/temporal/AbstractTemporalHelper.java
+++ src/de/cimt/talendcomp/db2/temporal/AbstractTemporalHelper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.jlo.talendcomp.db2.temporal;
+package de.cimt.talendcomp.db2.temporal;
 
 import java.io.BufferedWriter;
 import java.io.File;
